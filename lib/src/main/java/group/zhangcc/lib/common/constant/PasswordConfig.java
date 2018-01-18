2b15af1794df75147cfb6e4c29ef41fd6cd3ba20
q7208783 <576926338@qq.com>
2018-01-18 17:15:08 +0800

    remove

--- lib/src/main/java/group/zhangcc/lib/common/constant/PasswordConfig.java
+++ lib/src/main/java/group/zhangcc/lib/common/constant/PasswordConfig.java
@@ -6,21 +6,5 @@ package group.zhangcc.lib.common.constant;
 
 public class PasswordConfig {
 
-	public static final String aliyunAccessKeyId = "LTAIBRvqg88hSChh";
 
-	public static final String aliyunAccessSecret ="uCMiiS4of1LmAXh1eN974FKRkTSxmF";
-
-	public static final String datasourceUsername = "root";
-
-	public static final String datasourcePassword= "Zc57198083";
-
-	public static final String redisHost = "120.78.160.176";
-
-	public static final String redisPassword = "Zc57198083";
-
-	public static final int redisPort = 6379;
-
-	public static final String esHost = "120.78.160.176";
-
-	public static final int esPort = 9200;
 }
